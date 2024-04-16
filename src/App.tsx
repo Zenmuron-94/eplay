3174ce39a25c8297109ac9bc434914cd37b0ce41
Clovis Pujol <bioclp@hotmail.com>
2024-04-16 18:23:42 -0300

    finalizando parte 1 do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import Header from './components/Header'
+import { BrowserRouter } from 'react-router-dom'
+
+//import Header from './components/Header'
 import GlobalStyle from './styles'
+import Rotas from './routes'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyle />
-      <div className="container">
-        <Header />
-      </div>
-    </>
+      <Rotas />
+      <Footer />
+    </BrowserRouter>
   )
 }
 
